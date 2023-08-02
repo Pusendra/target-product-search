0eac30f54f044bdd5dbc125c3b4d4a6fd96e012f
pusendra <cpusendra@gmail.com>
2023-08-02 14:35:34 -0400

    Find Loader Element While data is loading from api on target.com

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,18 +1,43 @@
-const NavBar = ({ onSearch }) => {
+import { useState } from "react";
+
+const NavBar = ({ onSearch, loading }) => {
+  const [query, setQuery] = useState("");
+  const [formError, setFormError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (query.trim() === "") {
+      return setFormError("Please type anything to search");
+    }
+
+    onSearch(query);
+  };
+
   return (
     <div className="flex justify-between bg-neutral-400 py-5 px-2 rounded-sm">
       <h1 className="font-bold text-center flex items-center text-white">Search Target.com</h1>
-      <form className="flex items-center">
+      <form onSubmit={handleSubmit} className="flex items-center">
         <div className="flex flex-col">
           <input
             type="text"
-            className={`border p-2 mr-2 rounded-md `}
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+              setFormError("");
+            }}
+            className={`border p-2 mr-2 rounded-md ${formError ? "border-red-500 mt-4" : ""}`}
             placeholder="Type to search"
             style={{ width: "300px" }}
           />
+
+          <span className="text-red-500 text-sm text-left px-2">{formError}</span>
         </div>
-        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
-          Search
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          disabled={loading}>
+          {loading ? "Searching..." : "Search"}
         </button>
       </form>
     </div>
